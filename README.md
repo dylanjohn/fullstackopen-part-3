5534930577bf6c928f43d36c821fe8ebf98ac4e8
dylanjohn <dylan@mediamachine.co.za>
2024-08-10 11:45:34 +0200

    Add phonebook frontend

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # fullstackopen-part-3
 
 Node.js server using Express and MongoDB
+
+https://fullstackopen-part-3-1wp0.onrender.com
--- index.js
+++ index.js
@@ -34,6 +34,7 @@ morgan.token('req-body', (request) => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
@@ -110,7 +111,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
